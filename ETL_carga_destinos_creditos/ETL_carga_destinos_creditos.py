ecef6bf71fe8ac9d8e1d682bf3c7809d6388bac2
DataAnalitica-C <dataanalitica@bancocontactar.com>
2026-07-08 08:22:44 -0500

    Synchronizing job ETL_carga_destinos_creditos from AWS Glue at 2026/07/08 13:22:44

---
+++ ETL_carga_destinos_creditos/ETL_carga_destinos_creditos.py
@@ -0,0 +1,93 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+import boto3
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'connection_name'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+def read_using_jdbc(connection_name: str, custom_sql_query: str):
+    connection = glueContext.extract_jdbc_conf(connection_name=connection_name)
+    
+    spark_df = spark.read.format("jdbc").options(
+        url=connection['url'],
+        dbtable=f"({custom_sql_query}) AS subquery",
+        user=connection['user'],
+        password=connection['password'],
+        driver="com.microsoft.sqlserver.jdbc.SQLServerDriver"
+    ).load()
+
+    return DynamicFrame.fromDF(spark_df, glueContext, "dynamic_frame_source")
+
+def write_to_s3_parquet(dynamic_frame, s3_path, transformation_ctx="write_to_s3"):
+    glueContext.write_dynamic_frame.from_options(
+        frame=dynamic_frame,
+        connection_type="s3",
+        connection_options={"path": s3_path},
+        format="parquet",
+        format_options={"compression": "gzip"},
+        transformation_ctx=transformation_ctx
+    )
+
+def clear_s3_path(s3_path):
+    try:
+        if s3_path.startswith('s3://'):
+            s3_path = s3_path[5:]
+        bucket_name = s3_path.split('/')[0]
+        prefix = '/'.join(s3_path.split('/')[1:])
+        s3_client = boto3.client('s3')
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+        if 'Contents' in response:
+            objects_to_delete = [{'Key': obj['Key']} for obj in response['Contents']]
+            if objects_to_delete:
+                s3_client.delete_objects(Bucket=bucket_name, Delete={'Objects': objects_to_delete})
+    except Exception as e:
+        logger.warning(f"Could not clear S3 path: {str(e)}")
+
+def main():
+    try:
+        connection_name = args['connection_name']
+        s3_output_path = "s3://rawdatacontactar/fsd010_destinos_credito"
+        
+        sql_query = """
+        SELECT DISTINCT 
+            FS.Aocta,
+            FS.Aooper,
+            SG14.SNG014Dsc,
+            GETDATE() as Fecha
+        FROM FSD010 FS
+        INNER JOIN xwf700 XW ON FS.Aocta = XW.XWfCuenta 
+            AND FS.Aooper = XW.XWfOperacion 
+            AND FS.Aocta = XW.XWFCta1
+        INNER JOIN SNG001 SN ON XW.XWFPRCINS = SN.SNG001Inst 
+            AND XW.XWfCuenta = SN.SNG001cta
+        INNER JOIN SNG014 SG14 ON SN.SNG014COD = SG14.SNG014COD
+        """
+        
+        source_data = read_using_jdbc(connection_name, sql_query)
+        clear_s3_path(s3_output_path)
+        write_to_s3_parquet(source_data, s3_output_path, "write_fsd010_data")
+        
+        logger.info("ETL job completed successfully!")
+        
+    except Exception as e:
+        logger.error(f"ETL job failed: {str(e)}")
+        raise
+    finally:
+        job.commit()
+
+if __name__ == "__main__":
+    main()
